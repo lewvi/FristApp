3ee4e84d4a848496a5981ba52c76f533a4981dfc
lewvi <56486124+lewvi@users.noreply.github.com>
2020-07-25 22:22:29 +0700

    add fragment

---
+++ app/src/main/java/com/example/fristapp/ContactFragment.kt
@@ -0,0 +1,16 @@
+package com.example.fristapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+ class ContactFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        saveInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_contact, container, false)
+    }
+}
+
--- app/src/main/java/com/example/fristapp/MainActivity.kt
+++ app/src/main/java/com/example/fristapp/MainActivity.kt
@@ -1,24 +1,20 @@
 package com.example.fristapp
 
+import android.app.PendingIntent.getActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
 
-
-class MainActivity : AppCompatActivity(){
     private lateinit var drawer: DrawerLayout
 
-   /* //variables
-    var drawerLayout: DrawerLayout? = null
-    var navigationView: NavigationView? = null
-    var toolbar: Toolbar? = null
-    var menu: Menu? = null
-    var textView: TextView? = null
-*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,6 +23,9 @@ class MainActivity : AppCompatActivity(){
         setSupportActionBar(toolbar)
 
        drawer = findViewById(R.id.drawer_layout)
+
+        var navigationView :NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
 
        var toggle:ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawer, toolbar,
             R.string.navigation_draw_open,R.string.navigation_draw_close)
@@ -43,5 +42,26 @@ class MainActivity : AppCompatActivity(){
             super.onBackPressed()
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_person -> {
+                 supportFragmentManager.beginTransaction()
+                     .replace(R.id.fragment_container,ProfileFragment())
+                     .commit()
+            }
+            R.id.nav_contact -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container,ContactFragment())
+                    .commit()
+            }
+        }
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
+
+
 

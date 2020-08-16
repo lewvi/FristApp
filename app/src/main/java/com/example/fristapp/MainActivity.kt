720e008928b069143467c86de735788a18ed155f
lewvi <lew_rip@hotmail.com>
2020-08-16 20:14:35 +0700

    Edit MainActivity

--- app/src/main/java/com/example/fristapp/MainActivity.kt
+++ app/src/main/java/com/example/fristapp/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.example.fristapp
 
-import android.app.Activity
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.replace
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 
 
@@ -76,6 +68,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
             R.id.nav_addContact -> {
                 supportFragmentManager.beginTransaction()
+                    //.replace(R.id.fragment_container,AddContactFragment())
                     .commit()
                     setContentView(R.layout.add_contact)
             }
